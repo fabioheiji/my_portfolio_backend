760bdd7207ee39f9c986cf7a099cba7c8982b124
Fabio <fabioheiji@gmail.com>
2023-07-07 21:19:33 -0300

    fix: update ALLOWED_HOSTS to use Vercel

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'my-portfolio-backend-tau.vercel.app',
+]
 
 
 # Application definition
