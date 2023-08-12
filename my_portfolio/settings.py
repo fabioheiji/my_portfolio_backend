acb6b684de30f5697c7e0a6d14839c4fd1406f0b
Fabio <fabioheiji@gmail.com>
2023-08-12 01:02:45 -0300

    feature: docker configurate

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
+        "USER": env('USER_PSQL'),
+        "PASSWORD": env('PASSWORD_PSQL'),
         "HOST": "db",  # set in docker-compose.yml
         "PORT": 5432,  # default postgres port
     }
